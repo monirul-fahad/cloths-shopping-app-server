04a34aae2b58d955a6e7c6b0f721499c2d4a9a78
monirul-fahad <immonirulfahad@gmail.com>
2021-12-18 18:37:10 +0600

    mongo db added

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5uwr5.mongodb.net/e-commerce-app?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 async function run() {
   try {
     await client.connect();
