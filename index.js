86db09316b3a79c8f2e0a5de23c662137e092e2e
monirul-fahad <immonirulfahad@gmail.com>
2021-12-18 18:32:02 +0600

    backend setup

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
 const cors = require("cors");
-
-dotenv.config();
-
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("DB Connection Successfully!"))
-  .catch((err) => {
-    console.log(err);
-  });
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Backend server is running!");
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("sunglass_shop");
+    const productCollection = database.collection("products");
+  } finally {
+    //  await client.close()
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("sunglass shop server is running");
+});
+
+app.listen(port, () => {
+  console.log("server running at port", port);
 });
